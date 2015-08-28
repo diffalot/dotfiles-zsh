4f78e87b655a4bb385833cea70f803649a4f7cf6
Andrew Davis <andrew@diff.mx>
2015-08-27 21:13:11 -0500

    backup to rescue script backs up installed packages

--- home/.backup-to-rescue.sh
+++ home/.backup-to-rescue.sh
@@ -2,19 +2,23 @@
 
 sudo cryptsetup open --type luks /dev/sdb2 rescue
 
+echo "Password Accepted, waiting 5 seconds to transfer files"
+
 sleep 5
 
 sudo mount /dev/mapper/rescue-root /mnt
+
+pacman -Qe | awk '{print $1}' > /tmp/package_list.txt
+
+sudo mv /tmp/package_list.txt > /mnt/package_list.txt
 
 cd /home/diff
 
 sudo rsync -axX --exclude .cache --exclude .config/google-chrome --exclude '.vim/tmp/*' --exclude Videos --exclude Desktop --exclude 'build/linux-*' --progress --partial --delete /home/diff/ /mnt/home/diff/
 
-#sudo ls -alh /mnt/home
-#sudo ls -alh /mnt/home/diff
 sudo sync
 
-sudo umount -R /mnt
+sudo umount /mnt
 
 #sudo cryptosetup close /dev/mapper/rescue-root
 
