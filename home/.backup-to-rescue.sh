759b2a7702515bd2596319e493cbae6b37ff15db
Andrew Davis <andrew@diff.mx>
2015-08-27 21:22:22 -0500

    cleanup backup script and make it echo progress info to the terminal

--- home/.backup-to-rescue.sh
+++ home/.backup-to-rescue.sh
@@ -2,24 +2,32 @@
 
 sudo cryptsetup open --type luks /dev/sdb2 rescue
 
-echo "Password Accepted, waiting 5 seconds to transfer files"
+echo ""
+echo "Authentication Accepted"
+echo "Waiting 5 Seconds to Transfer Files"
+echo ""
 
 sleep 5
 
+echo "Mounting Rescue Volume"
+echo ""
 sudo mount /dev/mapper/rescue-root /mnt
 
-pacman -Qe | awk '{print $1}' > /tmp/package_list.txt
+echo "Copying List of Currently Installed Packages"
+echo ""
+pacman -Qe | awk '{print $1}' > /tmp/installed_packages.txt
+sudo mv /tmp/installed_packages.txt /mnt/sterling_package_list.txt
 
-sudo mv /tmp/package_list.txt > /mnt/package_list.txt
-
+echo "Copying Changed Files"
+echo ""
 cd /home/diff
-
 sudo rsync -axX --exclude .cache --exclude .config/google-chrome --exclude '.vim/tmp/*' --exclude Videos --exclude Desktop --exclude 'build/linux-*' --progress --partial --delete /home/diff/ /mnt/home/diff/
 
+echo "Unmounting Rescue Volume"
+echo ""
 sudo sync
-
 sudo umount /mnt
 
+# TODO figure out why devicemapper won't close the rescue lvm
 #sudo cryptosetup close /dev/mapper/rescue-root
-
 #sudo cryptsetup close /dev/mapper/rescue
